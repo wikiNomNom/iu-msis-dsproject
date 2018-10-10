49aa4415cec28d2f79751a34769ebaa2727f78d9
Rishabh Saxena <saxenarishab@yahoo.co.in>
2018-10-10 14:04:45 -0400

    typo in turbine

--- public/api/turbine.php
+++ public/api/turbine.php
@@ -4,7 +4,7 @@ require '../../app/common.php';
 
 
 // 1. Go to the database and get all work associated with the $taskId
-$tubrineArr = Turbine::getAllTurbines();
+$turbineArr = Turbine::getAllTurbines();
 
 // 2. Convert to JSON
 $json = json_encode($turbineArr, JSON_PRETTY_PRINT);
