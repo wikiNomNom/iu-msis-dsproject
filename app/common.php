9226136c149e7f4587ddcd15f40dda1b02e97a5d
Rishabh Saxena <saxenarishab@yahoo.co.in>
2018-10-10 14:00:35 -0400

    tubrine GET endpoint

--- app/common.php
+++ app/common.php
@@ -10,3 +10,4 @@ require 'environment.php';
 require 'models/Client.php';
 require 'models/Sensor.php';
 require 'models/Site.php';
+require 'models/Turbine.php';
---
+++ app/models/Turbine.php
@@ -0,0 +1,48 @@
+<?php
+
+class Turbine{
+
+  public $turbineId;
+  public $turbineName;
+  public $turbineDescription;
+  public $capacity;
+  public $rampUpTime;
+  public $maintenaceInterval;
+
+  public function __construct($row) {
+    $this->turbineId = intval($row['turbineId']);
+
+    $this->turbineName = $row['turbineName'];
+    $this->turbineDescription = $row['turbineDescription'];
+
+    $this->capacity = intval($row['capacity']);
+    $this->rampUpTime = intval($row['rampUpTime']);
+    $this->maintenaceInterval = intval($row['maintenaceInterval']);
+  }
+
+  public static function getAllTurbines() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM turbine';
+
+    $statement = $db->prepare($sql);
+
+    // 3. Run the query
+    $success = $statement->execute();
+
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      // 4.a. For each row, make a new work object
+      $turbineItem =  new Turbine($row);
+
+      array_push($arr, $turbineItem);
+    }
+
+    return $arr;
+  }
+
+
+}
