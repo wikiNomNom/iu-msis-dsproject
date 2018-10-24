000e45d22707c7613655907da475fd0e318ae58e
Rishabh Saxena <saxenarishab@yahoo.co.in>
2018-10-23 23:43:37 -0400

    q

--- public/api/site.php
+++ public/api/site.php
@@ -5,7 +5,7 @@ require '../../app/common.php';
 $clientId = intval($_GET['clientId'] ?? 0);
 
 // 1. Go to the database and get all work associated with the $taskId
-$siteArr = Site::getSiteByClientId($clientId);
+$siteArr = Site::getAllSites();
 
 // 2. Convert to JSON
 $json = json_encode($siteArr, JSON_PRETTY_PRINT);
