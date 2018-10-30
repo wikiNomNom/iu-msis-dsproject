8ae7264ea038c9fb379743661f7176332a6af242
Nick Magers <nmagers@iu.edu>
2018-10-29 21:03:35 -0400

    sensorkpi

---
+++ public/api/sensorKpi.php
@@ -0,0 +1,15 @@
+<?php
+
+require '../../app/common.php';
+
+$turbineId = intval($_GET['turbineId'] ?? 0);
+
+// 1. Go to the database and get all work associated with the $turbineId
+$sensorKPIArr = SensorKPI::getSensorKpiByTurbineId($turbineId);
+
+// 2. Convert to JSON
+$json = json_encode($sensorKPIArr, JSON_PRETTY_PRINT);
+
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
