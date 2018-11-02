7f90cd8280aa451b2e5665c0f8d5f453da42c27d
Nick Magers <nmagers@iu.edu>
2018-11-02 17:52:32 -0400

    chart

--- public/js/charts.app.js
+++ public/js/charts.app.js
@@ -307,6 +307,39 @@ var chartsApp = new Vue ({
                 }
 
             },
+           legend: {
+
+                enabled: false
+
+            },
+
+            plotOptions: {
+
+                area: {
+
+                    fillColor: {
+
+                        linearGradient: {
+
+                            x1: 0,
+
+                            y1: 0,
+
+                            x2: 0,
+
+                            y2: 1
+
+                        },
+
+                        stops: [
+
+                            [0,'#f8b195'],
+
+                            [1, '#f67280']
+
+                        ]
+
+                    },
 
 
     },//methods closed
